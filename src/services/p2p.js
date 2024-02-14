24f861c379c2434e911844b159a401241adadad5
agustinimaginagroup <agustin@imaginagroup.com>
2024-02-15 00:47:29 +0100

    broadcast y mensajeria entre conexiones

--- src/services/p2p.js
+++ src/services/p2p.js
@@ -1,6 +1,8 @@
 import WebSocket from 'ws';
 
-const { P2P_PORT = 5000 } = process.env;
+const { P2P_PORT = 9000, PEERS } = process.env;
+const peers = PEERS ? PEERS.split(',') : [];
+const MESSAGE = { BLOCKS: 'blocks' };
 
 class P2PService {
   constructor(blockchain) {
@@ -12,12 +14,34 @@ class P2PService {
     const server = new WebSocket.Server({ port: P2P_PORT });
 
     server.on('connection', (socket) => this.onConnection(socket));
+
+    peers.forEach((peer) => {
+      const socket = new WebSocket(peer);
+      socket.on('open', () => this.onConnection(socket));
+    });
+
     console.log('P2P Service listening on: ', P2P_PORT);
   }
 
   onConnection(socket) {
-    console.log('Connection stablished');
+    const { blockchain } = this;
+
+    console.log('[ws:socket] connected.');
     this.sockets.push(socket);
+    socket.on('message', (message) => {
+      const { type, value } = JSON.parse(message);
+
+      console.log({ type, value });
+    });
+
+    socket.send(JSON.stringify({ type: MESSAGE.BLOCKS, value: blockchain.blocks }));
+  }
+
+  broadcast(type, value) {
+    console.log(`[ws:broadcast] ${type}...`);
+    const message = JSON.stringify({ type, value });
+
+    this.sockets.forEach((socket) => socket.send(message));
   }
 }
 
